b8d54a8bc0d535f61c5dec62a90aca84daf26c77
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-05-02 17:19:53 +1000

    add cards.go

---
+++ blackjack/cards.go
@@ -0,0 +1,27 @@
+package blackjack
+
+type Card struct {
+	Face FaceDat
+	Suit Suit
+}
+
+type Suit = int
+
+const (
+	SUIT_HEART Suit = iota
+	SUIT_DIAMOND
+	SUIT_SPADE
+	SUIT_CLUBS
+)
+
+type FaceDat struct {
+	value int
+}
+
+func (f *FaceDat) Value() int {
+	return f.value
+}
+
+func (c *Card) IsRed() bool {
+	return c.Suit == SUIT_DIAMOND || c.Suit == SUIT_HEART
+}
