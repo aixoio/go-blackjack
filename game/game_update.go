09c80c92659dbe9060d296a8d862d9b5e84479e2
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-05-04 14:24:42 +1000

    draw button to screen

--- game/game_update.go
+++ game/game_update.go
@@ -1,5 +1,9 @@
 package game
 
 func (g *Game) Update() error {
+	if CURRENT_STATE == HOME {
+		UpdateHome()
+	}
+
 	return nil
 }
--- game/home_draw.go
+++ game/home_draw.go
@@ -8,4 +8,22 @@ import (
 
 func DrawHome(screen *ebiten.Image) {
 	DrawTextWithPoppinsRegularAt("Blackjack", color.White, 200, 75, screen, 48)
+	DrawHomePlayButton(screen)
+}
+
+// Button location: 268, 300
+//
+// Button dimenstions: 128x32
+func DrawHomePlayButton(screen *ebiten.Image) {
+	locationGeoM := ebiten.GeoM{}
+
+	locationGeoM.Translate(268, 300)
+
+	btnImg := ebiten.NewImage(128, 32)
+	btnImg.Fill(color.Black)
+	DrawTextWithPoppinsRegularAt("PLAY", color.White, 48, 4, btnImg, 18)
+
+	screen.DrawImage(btnImg, &ebiten.DrawImageOptions{
+		GeoM: locationGeoM,
+	})
 }
