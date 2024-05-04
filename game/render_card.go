08f3e9303f6345e91f13c908b093d04bd387e1a7
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-05-04 12:05:50 +1000

    add full card rendering

--- game/render_card.go
+++ game/render_card.go
@@ -24,6 +24,27 @@ func DrawCard(screen *ebiten.Image, x, y int, card blackjack.Card) {
 		case blackjack.QUEEN_DAT:
 			DrawImageAt(x, y, screen, img.CLUBS_QUEEN)
 		}
+		switch card.Face.Value() {
+		case 2:
+			DrawImageAt(x, y, screen, img.CLUBS_2)
+		case 3:
+			DrawImageAt(x, y, screen, img.CLUBS_3)
+		case 4:
+			DrawImageAt(x, y, screen, img.CLUBS_4)
+		case 5:
+			DrawImageAt(x, y, screen, img.CLUBS_5)
+		case 6:
+			DrawImageAt(x, y, screen, img.CLUBS_6)
+		case 7:
+			DrawImageAt(x, y, screen, img.CLUBS_7)
+		case 8:
+			DrawImageAt(x, y, screen, img.CLUBS_8)
+		case 9:
+			DrawImageAt(x, y, screen, img.CLUBS_9)
+		case 10:
+			DrawImageAt(x, y, screen, img.CLUBS_10)
+
+		}
 	case blackjack.SUIT_SPADE:
 		switch card.Face.ExtraFaceDat() {
 		case blackjack.ACE_DAT:
@@ -35,7 +56,27 @@ func DrawCard(screen *ebiten.Image, x, y int, card blackjack.Card) {
 		case blackjack.QUEEN_DAT:
 			DrawImageAt(x, y, screen, img.SPADES_QUEEN)
 		}
-		return
+		switch card.Face.Value() {
+		case 2:
+			DrawImageAt(x, y, screen, img.SPADES_2)
+		case 3:
+			DrawImageAt(x, y, screen, img.SPADES_3)
+		case 4:
+			DrawImageAt(x, y, screen, img.SPADES_4)
+		case 5:
+			DrawImageAt(x, y, screen, img.SPADES_5)
+		case 6:
+			DrawImageAt(x, y, screen, img.SPADES_6)
+		case 7:
+			DrawImageAt(x, y, screen, img.SPADES_7)
+		case 8:
+			DrawImageAt(x, y, screen, img.SPADES_8)
+		case 9:
+			DrawImageAt(x, y, screen, img.SPADES_9)
+		case 10:
+			DrawImageAt(x, y, screen, img.SPADES_10)
+
+		}
 	case blackjack.SUIT_DIAMOND:
 		switch card.Face.ExtraFaceDat() {
 		case blackjack.ACE_DAT:
@@ -47,7 +88,29 @@ func DrawCard(screen *ebiten.Image, x, y int, card blackjack.Card) {
 		case blackjack.QUEEN_DAT:
 			DrawImageAt(x, y, screen, img.DIAMONDS_QUEEN)
 		}
-		return
+
+		switch card.Face.Value() {
+		case 2:
+			DrawImageAt(x, y, screen, img.DIAMONDS_2)
+		case 3:
+			DrawImageAt(x, y, screen, img.DIAMONDS_3)
+		case 4:
+			DrawImageAt(x, y, screen, img.DIAMONDS_4)
+		case 5:
+			DrawImageAt(x, y, screen, img.DIAMONDS_5)
+		case 6:
+			DrawImageAt(x, y, screen, img.DIAMONDS_6)
+		case 7:
+			DrawImageAt(x, y, screen, img.DIAMONDS_7)
+		case 8:
+			DrawImageAt(x, y, screen, img.DIAMONDS_8)
+		case 9:
+			DrawImageAt(x, y, screen, img.DIAMONDS_9)
+		case 10:
+			DrawImageAt(x, y, screen, img.DIAMONDS_10)
+
+		}
+
 	case blackjack.SUIT_HEART:
 		switch card.Face.ExtraFaceDat() {
 		case blackjack.ACE_DAT:
@@ -59,7 +122,29 @@ func DrawCard(screen *ebiten.Image, x, y int, card blackjack.Card) {
 		case blackjack.QUEEN_DAT:
 			DrawImageAt(x, y, screen, img.HEARTS_QUEEN)
 		}
-		return
+
+		switch card.Face.Value() {
+		case 2:
+			DrawImageAt(x, y, screen, img.HEARTS_2)
+		case 3:
+			DrawImageAt(x, y, screen, img.HEARTS_3)
+		case 4:
+			DrawImageAt(x, y, screen, img.HEARTS_4)
+		case 5:
+			DrawImageAt(x, y, screen, img.HEARTS_5)
+		case 6:
+			DrawImageAt(x, y, screen, img.HEARTS_6)
+		case 7:
+			DrawImageAt(x, y, screen, img.HEARTS_7)
+		case 8:
+			DrawImageAt(x, y, screen, img.HEARTS_8)
+		case 9:
+			DrawImageAt(x, y, screen, img.HEARTS_9)
+		case 10:
+			DrawImageAt(x, y, screen, img.HEARTS_10)
+
+		}
+
 	}
 
 }
