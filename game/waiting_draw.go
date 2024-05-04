710e7162bc2be4c9123f4c78de49b04ca3e91e9b
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-05-04 20:06:06 +1000

    add basic hit and stand buttons

--- game/waiting_draw.go
+++ game/waiting_draw.go
@@ -22,4 +22,46 @@ func DrawWaiting(screen *ebiten.Image) {
 	}
 
 	DrawTextWithPoppinsRegularAt(fmt.Sprintf("%d", PlayerCount), color.White, 30, 588, screen, 24)
+	DrawHitButton(screen)
+	DrawStandButton(screen)
+}
+
+// Button Location: 80, 588
+//
+// Button dimensions: 128x32
+func DrawHitButton(screen *ebiten.Image) {
+	locationGeoM := ebiten.GeoM{}
+	locationGeoM.Translate(80, 588)
+	btnImg := ebiten.NewImage(128, 32)
+	btnImg.Fill(color.RGBA{R: 51, G: 51, B: 51, A: 255})
+	if MouseIsOverHitButton {
+		btnImg.Fill(color.RGBA{R: 150, G: 150, B: 150, A: 255})
+		DrawTextWithPoppinsRegularAt("HIT", color.RGBA{R: 51, G: 51, B: 51, A: 255}, 48, 4, btnImg, 18)
+	} else {
+		DrawTextWithPoppinsRegularAt("HIT", color.White, 48, 4, btnImg, 18)
+	}
+
+	screen.DrawImage(btnImg, &ebiten.DrawImageOptions{
+		GeoM: locationGeoM,
+	})
+}
+
+// Button Location: 240, 588
+//
+// Button dimensions: 128x32
+func DrawStandButton(screen *ebiten.Image) {
+	locationGeoM := ebiten.GeoM{}
+	locationGeoM.Translate(240, 588)
+	btnImg := ebiten.NewImage(128, 32)
+	btnImg.Fill(color.RGBA{R: 51, G: 51, B: 51, A: 255})
+	if MouseIsOverStandButton {
+		btnImg.Fill(color.RGBA{R: 150, G: 150, B: 150, A: 255})
+		DrawTextWithPoppinsRegularAt("STAND", color.RGBA{R: 51, G: 51, B: 51, A: 255}, 32, 4, btnImg, 18)
+	} else {
+		DrawTextWithPoppinsRegularAt("STAND", color.White, 32, 4, btnImg, 18)
+	}
+
+	screen.DrawImage(btnImg, &ebiten.DrawImageOptions{
+		GeoM: locationGeoM,
+	})
 }
--- game/waiting_update.go
+++ game/waiting_update.go
@@ -1,5 +1,9 @@
 package game
 
-func WaitingUpdate() {
+var MouseIsOverHitButton = false
+var MouseIsOverStandButton = false
 
+func WaitingUpdate() {
+	MouseIsOverHitButton = MouseIsOver(80, 588, 128, 32)
+	MouseIsOverStandButton = MouseIsOver(240, 588, 128, 32)
 }
