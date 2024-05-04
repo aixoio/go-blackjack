d3c4f1569024cc5e33dd71e832d64f8587c15332
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-05-04 20:09:52 +1000

    add basic action to hit me button

--- game/waiting_update.go
+++ game/waiting_update.go
@@ -1,9 +1,19 @@
 package game
 
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+)
+
 var MouseIsOverHitButton = false
 var MouseIsOverStandButton = false
 
 func WaitingUpdate() {
 	MouseIsOverHitButton = MouseIsOver(80, 588, 128, 32)
 	MouseIsOverStandButton = MouseIsOver(240, 588, 128, 32)
+
+	if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) && MouseIsOverHitButton {
+		HitMe()
+	}
+
 }
