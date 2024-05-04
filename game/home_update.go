137a0f57c49680c352fa5c13e2a30bda214e8213
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-05-04 14:38:26 +1000

    make button switch state

--- game/home_update.go
+++ game/home_update.go
@@ -1,7 +1,14 @@
 package game
 
+import "github.com/hajimehoshi/ebiten/v2"
+
 var MouseIsOverHomeButton = false
 
 func UpdateHome() {
 	MouseIsOverHomeButton = MouseIsOver(268, 300, 128, 32)
+
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) && MouseIsOverHomeButton {
+		CURRENT_STATE = WAITING
+	}
+
 }
